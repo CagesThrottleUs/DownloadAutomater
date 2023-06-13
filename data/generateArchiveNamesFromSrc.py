d2e21dda7f8b0d4bf1b0e864d23061398007b368
Lakshya <manstein.felix@gmail.com>
2023-06-13 12:00:02 +0530

    rename archive name generator file and add comments
    The file was renamed from generateArchiveName.py to generateArchiveNameFromSrc.py.
    
    Also updated the executable options.
    
    Signed-off-by: Lakshya <manstein.felix@gmail.com>

---
+++ data/generateArchiveNamesFromSrc.py
@@ -0,0 +1,21 @@
+# This files main goal is to fill from A source Location. This is the first time script as the next time,
+# only the Trie data will be used to upgrade the data file directly as it is closer to program.
+#
+# This may not be required in case you are creating your own new playlist. Just start downloading.
+#
+# To run - python ./data/generateArchiveNames.py <folderLocation> <archiveName> at Github Directory level.
+#
+# Prerequisites - Python3, Natsort
+
+import os
+from natsort import os_sorted
+import sys
+
+source = sys.argv[1]
+files = os.listdir(source)
+files = os_sorted(files)
+with open(sys.argv[2], mode="w", encoding="utf-8") as f:
+    for file in files:
+        file = file.removesuffix(".mp3")
+        print(file)
+        print(file, file=f)
